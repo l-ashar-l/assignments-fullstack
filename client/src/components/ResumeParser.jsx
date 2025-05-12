416f59fc7ce259e4c5257e417c895b55ef6a0a5f
Ashar <asharansaricr7@gmail.com>
2025-05-13 04:07:55 +0530

    refactoring server code

--- client/src/components/ResumeParser.jsx
+++ client/src/components/ResumeParser.jsx
@@ -11,7 +11,7 @@ export default function ResumeParser() {
     setError('');
     setIsLoading(true);
     try {
-      const res = await fetch('http://localhost:9000/api/parse', {
+      const res = await fetch('http://localhost:9000/api/resume/parse', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text }),
